ccc1b395b5250abeeb3ed1ab96f1ef6815172c10
Bronson Naylor <bronsonnaylor@gmail.com>
2020-08-25 21:36:31 -0600

    AJAX request implimented and rendered.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,33 @@
-let btn = document.getElementById("button");
+let pageCounter = 1;
+let animalContainer = document.getElementById("animal-info")
+let btn = document.getElementById("btn");
 
 btn.addEventListener("click", function() {
     let myRequest = new XMLHttpRequest();
-    myRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+    myRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-' + pageCounter + '.json');
     myRequest.onload = function() {
         let myData = JSON.parse(myRequest.responseText);
-        console.log(myData[0]);
+        renderHTML(myData);
     };
-    myRequest.send(); 
+    myRequest.send();
+    pageCounter++;
+    if (pageCounter > 3) {
+        btn.style.display = "none";
+    }
 });
+
+function renderHTML(data) {
+    let HTMLString = "";
+    for (i = 0; i < data.length; i++) {
+        HTMLString += "<p>" + data[i].name + " is a " + data[i].species + " that likes to eat "
+        for (ii = 0; ii < data[i].foods.likes.length; ii++) {
+            if (ii == 0) {
+                HTMLString += data[i].foods.likes[ii];
+            } else {
+                HTMLString += " and " + data[i].foods.likes[ii];
+            }
+        }
+        HTMLString += ".</p>";
+    }
+    animalContainer.insertAdjacentHTML('beforeend', HTMLString);
+}
